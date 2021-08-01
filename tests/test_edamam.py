c6017c0891fa0b3e81e1b8985bb558f3e639e915
J. Garcia <janikgar@gmail.com>
2021-08-01 10:53:58 -0700

    reorganize with sane package name

--- tests/test_edamam.py
+++ tests/test_edamam.py
@@ -1,5 +1,12 @@
 import pytest
+from edamam_flask import app
 
 @pytest.fixture
-def app():
-    app = edamam
+def client():
+    test_app = app.create_app()
+    with test_app.test_client() as client:
+        yield client
+
+def test_root(client):
+    request = client.get('/')
+    assert request.status_code == 200
