4ce266fff3c00fc7f7be9b41c49ae83499b540eb
J. Garcia <janikgar@gmail.com>
2021-08-01 15:13:52 -0700

    wrap w/ OpenTelemetry

--- edamam_flask/app.py
+++ edamam_flask/app.py
@@ -1,12 +1,33 @@
 from flask import Flask, make_response
+from flask.cli import load_dotenv
 from flask_restx import Resource, Api
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.resources import Resource as TraceResource
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter  
 from edamam_flask.edamam import get_upc, get_ingredient
 
 def create_app(test_config=None) -> Flask:
     app = Flask(__name__)
+    trace.set_tracer_provider(TracerProvider(
+        resource=TraceResource.create({'service.name':'edamam_flask'})
+    ))
+    trace.get_tracer_provider().add_span_processor(
+        # BatchSpanProcessor(ConsoleSpanExporter())
+        BatchSpanProcessor(JaegerExporter(
+            agent_host_name='localhost',
+            agent_port=6831,
+        ))
+    )
+    FlaskInstrumentor().instrument_app(app)
+    RequestsInstrumentor().instrument()
+    tracer = trace.get_tracer_provider().get_tracer(__name__)
     api = Api(app)
 
-    @api.route( '/upc/<upc>')
+    @api.route('/upc/<upc>')
     class Upc(Resource):
         def get(self, upc: str):
             return get_upc(upc)
@@ -16,8 +37,5 @@ def create_app(test_config=None) -> Flask:
         def get(self, ingredient: str):
             return get_ingredient(ingredient)
     
+    load_dotenv()
     return app
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
